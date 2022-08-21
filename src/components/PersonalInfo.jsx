236e5f0b7acd009872a2eb6e4291c164e05c47d9
PurityGwaro <puritygwaro@gmail.com>
2022-08-21 23:15:17 +0300

    edit on media query to allow mobile responsiveness

--- src/components/PersonalInfo.jsx
+++ src/components/PersonalInfo.jsx
@@ -1,39 +1,52 @@
+import { useState,useEffect } from 'react'
+ 
 function PersonalInfo() {
+    const [form, setForm] = useState({});
+
+    const handleInputChange = (event) => {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+    
+        setForm({
+          [name]: value
+        });
+      }
+    
+    useEffect(()=>{
+        console.log({form});
+    }, [form]);
+
   return (
     <form>
         <div className="form-group">
             <label htmlFor="firstName">Name</label>
-            <input type="text" id="name" placeholder="Jane Doe" />
+            <input type="text" name="name" id="name" placeholder="Jane Doe" value={form.name} onChange={(e) => handleInputChange(e)}/>
         </div>
         <div className="form-group">
             <label htmlFor="lastName">Email Address<sup>*</sup></label>
             <small className="email-description">The purchase receipt would be sent to this address</small>
-            <input type="text" id="lastName" placeholder="Last Name" />
+            <input type="text" name="email" id="lastName" placeholder="janedoe@..." onChange={(e) => handleInputChange(e)}/>
         </div>
         <div className="form-group">
-            <label htmlFor="email">Address 1</label>
-            <input type="email" id="email" placeholder="Email" />
+            <label htmlFor="address1">Address 1</label>
+            <input type="text" id="address1" placeholder="address1" name='address1' onChange={(e) => handleInputChange(e)}/>
         </div>
         <div className="form-group">
-            <label htmlFor="phone">Address 2</label>
-            <input type="text" id="phone" placeholder="Phone" />
+            <label htmlFor="address2">Address 2</label>
+            <input type="text" id="address2" placeholder="address2" name='address2' onChange={(e) => handleInputChange(e)}/>
         </div>
         <div className="state">
                 {/* setup country selection  */}
             <div className="form-group">
                 <label htmlFor="country">Local Government</label>
                 {/* input country name */}
-                <input type="text" id="country" placeholder="Nigeria" />
+                <input type="text" id="country" placeholder="Nigeria" name='country' onChange={(e) => handleInputChange(e)}/>
             </div>
-            {/* input state in the selected country
+          
             <div className="form-group">
                 <label htmlFor="state">State</label>
-                <input type="text" className="form-control" id="state" placeholder="State" />
-            </div> */}
-            {/* dropdown input for selecting the state of the country given */}
-            <div className="form-group">
-                <label htmlFor="state">State</label>
-                <select className="form-control" id="state">
+                <select className="form-control" id="state" name='state' onChange={(e) => handleInputChange(e)}>
                     <option>Select State</option>
                     <option>Abia</option>
                     <option>Adamawa</option>
